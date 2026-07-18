ed2ea71694f4887f62ed59e1965bab524a80463a
Bijoy Islam <bijoy.b19818686@gmail.com>
2026-07-19 00:21:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 const sidebar = document.getElementById("sidebar");
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const themeBtn = document.getElementById("themeBtn");
 
-document.getElementById("menuBtn").onclick = function () {
-    sidebar.classList.add("active");
+// Open & Close Sidebar
+menuBtn.onclick = function () {
+    sidebar.classList.toggle("active");
 };
 
-document.getElementById("closeBtn").onclick = function () {
+closeBtn.onclick = function () {
     sidebar.classList.remove("active");
 };
 
-document.getElementById("themeBtn").onclick = function () {
+// Click outside sidebar = Close
+document.addEventListener("click", function (e) {
+    if (
+        !sidebar.contains(e.target) &&
+        !menuBtn.contains(e.target)
+    ) {
+        sidebar.classList.remove("active");
+    }
+});
+
+// Dark Mode
+themeBtn.onclick = function () {
     document.body.classList.toggle("dark");
 
-    if(document.body.classList.contains("dark")){
+    if (document.body.classList.contains("dark")) {
         this.innerHTML = "☀️";
-    }else{
+    } else {
         this.innerHTML = "🌙";
+    }
+};
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+    themeBtn.innerHTML = "☀️";
+}
+
+themeBtn.onclick = function () {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        this.innerHTML = "☀️";
+        localStorage.setItem("theme", "dark");
+    } else {
+        this.innerHTML = "🌙";
+        localStorage.setItem("theme", "light");
     }
 };
